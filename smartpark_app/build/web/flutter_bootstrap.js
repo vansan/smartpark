d3291cacd0a5a4b5d0c95014f6c71d00087afc54
vansan <vansan@users.noreply.github.com>
2026-06-08 19:27:34 +0530

    fix: register url_launcher_web package directly to resolve MissingPluginException on web

--- smartpark_app/build/web/flutter_bootstrap.js
+++ smartpark_app/build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "117350623" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2973454698" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
